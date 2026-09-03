0387f9d2db6680a52df4cb7390f640911e835d61
DSamuelHernadnez <dpina5596@gmail.com>
2026-09-03 16:14:14 -0500

    feat(nav): Adds a link to the application logo, setting a target and its interactive style for the logo

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar';
 import styles from './NavBar.module.css';
 
@@ -7,7 +7,9 @@ const Nav = () => {
    return (
       <header className={styles.navContainer}>
          <div className={styles.brandWrapper}>
-            <span className={styles.brandLogo}>WorderMark</span>
+            <Link to="/" className={styles.brandLink}>
+               <span className={styles.brandLogo}>WorderMark</span>
+            </Link>
          </div>
 
          <nav className={styles.linksGroup}>
