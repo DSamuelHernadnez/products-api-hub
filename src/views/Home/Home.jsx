cb9aa8205401b49f893156b311d89431083512d7
DSamuelHernadnez <dpina5596@gmail.com>
2026-09-11 17:46:10 -0500

    feat(Home): integrar el componente Filter en la vista principal

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Filter from '../../components/Filter/Filter';
 import Cards from '../../components/Cards/Cards';
 import { getProducts } from '../../redux/actions';
 import styles from './Home.module.css';
@@ -15,18 +16,18 @@ const Home = () => {
       }
    }, [dispatch, products.length]);
 
-   return (
+return (
       <main className={styles.homeContainer}>
+         <Filter />
          <header className={styles.homeHeader}>
             <h1 className={styles.homeTitle}>
                EXPLORA NUESTROS PRODUCTOS
             </h1>
-
             <p className={styles.homeSubtitle}>
                Calidad y variedad excepcional
             </p>
          </header>
-
+         {/* Insertamos los filtros únicamente Home */}
          <Cards products={products} />
       </main>
    );
